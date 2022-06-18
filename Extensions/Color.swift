81ebe17d50198e032d067bf3733c83251e96e56d
fischerweise <mustafaapekdemir@gmail.com>
2022-06-18 16:09:59 +0300

    project theme created

---
+++ Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  Cheetah
+//
+//  Created by Mustafa Pekdemir on 18.06.2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
