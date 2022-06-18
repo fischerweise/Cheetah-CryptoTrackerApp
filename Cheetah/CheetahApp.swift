557cb70e55820c7877aa84ab8076ef9e73e3cdad
fischerweise <mustafaapekdemir@gmail.com>
2022-06-18 15:50:28 +0300

    Project created

---
+++ Cheetah/CheetahApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheetahApp.swift
+//  Cheetah
+//
+//  Created by Mustafa Pekdemir on 18.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CheetahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
