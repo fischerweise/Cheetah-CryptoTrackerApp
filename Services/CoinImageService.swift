b3863757463e6fb064188f335297792a14bc5f36
fischerweise <mustafaapekdemir@gmail.com>
2022-06-19 14:30:10 +0300

    file manager created

--- Services/CoinImageService.swift
+++ Services/CoinImageService.swift
@@ -14,11 +14,24 @@ class CoinImageService {
     @Published var image: UIImage? = nil
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     init(coin: CoinModel) {
         self.coin = coin
-        getCoinsImage()
+        self.imageName = coin.id
+        getCoinImage()
     }
-    private func getCoinsImage() {
+    private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Retrieved from File Manager")
+        } else {
+            downloadCoinImage()
+            print("Downloading Image.")
+        }
+    }
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else {
             return
         }
@@ -27,8 +40,12 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else {
+                    return
+                }
+                self.image = returnedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
